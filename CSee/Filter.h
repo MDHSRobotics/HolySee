5b4f7ea741d15755f8835dea624e2c7926858dfa
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-03-02 13:04:54 -0800

    added filterpostevent

--- CSee/Filter.h
+++ CSee/Filter.h
@@ -3,16 +3,20 @@
 
 #include <string>
 #include "opencv2/opencv.hpp"
+#include "gst/gst.h"
+
 
 class Filter
 {
 private:
 	std::string &name;
-	
+	GstBus* bus;
 public:
 	Filter(std::string&);
 	virtual void process(cv::Mat&) = 0;
 	std::string& getName();
+	void setBus(GstBus* bus);
+	void post(GstStructure *);
 };
 
 #endif /* __GST_ARFILTER_FILTER__ */
