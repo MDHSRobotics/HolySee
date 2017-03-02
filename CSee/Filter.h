8474e83c3edbd7ae8cf6a79b0c0890ab04b1f327
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-03-02 13:59:08 -0800

    added target acquisition

--- CSee/Filter.h
+++ CSee/Filter.h
@@ -17,6 +17,11 @@ public:
 	std::string& getName();
 	void setBus(GstBus* bus);
 	void post(GstStructure *);
+	void postBooleanEvent(std::string eventName, bool eventData);
+	void postStringEvent(std::string eventName, std::string eventData);
+	void postIntEvent(std::string eventName, int eventData);
+	void postDoubleEvent(std::string eventName, double eventData);
+	
 };
 
 #endif /* __GST_ARFILTER_FILTER__ */
