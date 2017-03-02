f009941689c86cad7293343c1456131b7acd931d
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-03-02 13:58:18 -0800

    added targetacquisition

--- CSee/Filter.cpp
+++ CSee/Filter.cpp
@@ -18,3 +18,23 @@ void Filter::post(GstStructure * msgStruct){
 	GstMessage *msg = gst_message_new_application(GST_OBJECT(bus), msgStruct);
 	gst_bus_post(bus, msg);
 }
+
+void Filter::postBooleanEvent(std::string eventName, bool eventData){
+	GstStructure* msgStruct = gst_structure_new(eventName.c_str(), "filter", G_TYPE_STRING, getName().c_str(), "eventData", G_TYPE_BOOLEAN, eventData, NULL);
+	post(msgStruct);
+}
+
+void Filter::postStringEvent(std::string eventName, std::string eventData){
+	GstStructure* msgStruct = gst_structure_new(eventName.c_str(), "filter", G_TYPE_STRING, getName().c_str(), "eventData", G_TYPE_STRING, eventData, NULL);
+	post(msgStruct);
+}
+
+void Filter::postIntEvent(std::string eventName, int eventData){
+	GstStructure* msgStruct = gst_structure_new(eventName.c_str(), "filter", G_TYPE_STRING, getName().c_str(), "eventData", G_TYPE_INT, eventData, NULL);
+	post(msgStruct);
+}
+
+void Filter::postDoubleEvent(std::string eventName, double eventData){
+	GstStructure* msgStruct = gst_structure_new(eventName.c_str(), "filter", G_TYPE_STRING, getName().c_str(), "eventData", G_TYPE_DOUBLE, eventData, NULL);
+	post(msgStruct);
+}
