520d834dbb13f63c5972d7117944d21119753309
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-03-02 14:16:20 -0800

    fixed naming bug

--- CSee/gstlidarsrc.h
+++ CSee/gstlidarsrc.h
@@ -36,7 +36,7 @@ G_BEGIN_DECLS
 	(G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_LIDAR_SRC, GstLidarSrcClass))
 #define GST_IS_LIDAR_SRC(obj) \
 	(G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_LIDAR_SRC))
-#define GST_IS_SHM_SRC_CLASS(klass) \
+#define GST_IS_LIDAR_SRC_CLASS(klass) \
 	(G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_LIDAR_SRC))
 typedef struct _GstLidarSrc GstLidarSrc;
 typedef struct _GstLidarSrcClass GstLidarSrcClass;
