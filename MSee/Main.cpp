ecc1b0f5228d0310dd73867985c7b3a6fd580901
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-03-03 09:48:11 -0800

    added console connection notfication

--- MSee/Main.cpp
+++ MSee/Main.cpp
@@ -75,7 +75,7 @@ int main(int argc,char *argv[])
 	std::thread keyboardListener(processKeys,msee);
 	keyboardListener.detach();
 	
-	while(!msee->isReady()){
+	while(!MSee::streamer->isReady()){
 		std::this_thread::sleep_for(std::chrono::milliseconds(100)); //Unix
 	}
 	
